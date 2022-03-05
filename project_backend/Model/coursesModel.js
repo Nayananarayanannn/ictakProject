90e0d0c1f918937f142118c001ae16cd768dd0eb
Nayana Narayanan <nayananarayanannn@gmail.com>
2022-03-05 17:32:09 +0530

    codes commenting

--- project_backend/Model/coursesModel.js
+++ project_backend/Model/coursesModel.js
@@ -1,34 +1,63 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 // mongodb+srv://Nayana:Nayana95@ictaktrial.nyh4v.mongodb.net/my-blog?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://ictpro2022:Ict2022@cluster0.bjv0s.mongodb.net/ICTProject',{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
-})
-.then(()=>{
-    console.log('mongoDB connected...')
-})
-.catch(err=>{
-    console.log('connection failed!', err.stack)
-})
- const Schema= mongoose.Schema;
+const Schema = mongoose.Schema;
 
- var Courses= new Schema({
-     
-     url:{ type: String, required: true },
-     name:{ type: String, required: true },
-     status:{ type: String, required: true },
-     title:{ type: String, required: true },
-     quote:{ type: String, required: true },
-     shortDescription:{ type: String, required: true },
-     objectives:{ type: Array, required: true },
-     highlights:{ type: Array, required: true },
-     agenda:{ type: Array, required: true },
-     eligibility:{ type: Array, required: true },
-     fee:{ type: Number, required: true },
-     dates:{ type: Array, required: true },
-     image:{ type:String,required: true }
- });
+// courses schema
+var Courses = new Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  quote: {
+    type: String,
+    required: true,
+  },
+  shortDescription: {
+    type: String,
+    required: true,
+  },
+  objectives: {
+    type: Array,
+    required: true,
+  },
+  highlights: {
+    type: Array,
+    required: true,
+  },
+  agenda: {
+    type: Array,
+    required: true,
+  },
+  eligibility: {
+    type: Array,
+    required: true,
+  },
+  fee: {
+    type: Number,
+    required: true,
+  },
+  dates: {
+    type: Array,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
 
- var coursesModel=mongoose.model('courses',Courses);
+var coursesModel = mongoose.model("courses", Courses);
 
- module.exports= coursesModel;
+module.exports = coursesModel;
--- project_backend/server.js
+++ project_backend/server.js
@@ -1,40 +1,58 @@
-const express = require('express');
-const Courses = require('./Model/coursesModel');
+const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const courseRouter = require("./routes/courseRouter");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors()); //set cors
 
+// cors headers
 app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "*");
-  
-    // Request methods you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,POST,OPTIONS,PUT,PATCH,DELETE"
-    );
-  
-    // Request headers you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-with,content-type"
-    );
-  
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-  
-    // Pass to next layer of middleware
-    next();
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS,PUT,PATCH,DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-with,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// mongodb connection
+mongoose
+  .connect(
+    "mongodb+srv://ictpro2022:Ict2022@cluster0.bjv0s.mongodb.net/ICTProject",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("mongoDB connected...");
+  })
+  .catch((err) => {
+    console.log("connection failed!", err.stack);
   });
 
-  const courseRouter = require('./routes/courseRouter');
-  app.use('/api/courses',courseRouter)
+// routes
+app.use("/api/courses", courseRouter);
 
-
-app.listen(process.env.PORT||8000,()=>{
-    console.log('Server running on port 8000...')
-})
+// listen port
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server running on port 8000...");
+});
