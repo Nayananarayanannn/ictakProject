028748d93cd075038d552903c1b923414929005d
Nayana Narayanan <nayananarayanannn@gmail.com>
2022-03-08 13:29:54 +0530

    Navbar updated

--- project_frontend/src/components/NavBar.js
+++ project_frontend/src/components/NavBar.js
@@ -31,9 +31,9 @@ const NavBar = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
           <svg
@@ -43,9 +43,9 @@ const NavBar = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </button>
@@ -93,13 +93,20 @@ const NavBar = () => {
               </a>
             </li>
             <li>
-              <a
-                href="/membership"
-                className="block py-2 pr-4 pl-3 text-gray-100 border-b border-gray-100 hover:bg-gray-500 md:hover:bg-transparent md:border-0 md:hover:text-[#5297E0] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Membership
-              </a>
-            </li>
+            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-100 border-b border-gray-100 hover:bg-gray-500 md:hover:bg-transparent md:border-0 md:hover:text-[#5297E0] md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Membership <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
+           
+            <div id="dropdownNavbar" class="hidden z-10 w-44 text-base list-none bg-[#111827] rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                <ul class="py-1" aria-labelledby="dropdownLargeButton">
+                  <li>
+                    <a href="/membership/academic" class="block py-2 px-4 text-sm text-gray-100 hover:bg-gray-500 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Academic</a>
+                  </li>
+                  <li>
+                    <a href="/membership/corporate" class="block py-2 px-4 text-sm text-gray-100 hover:bg-gray-500 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Corporate</a>
+                  </li>
+                </ul>
+                
+            </div>
+        </li>
             <li>
               <a
                 href="/events"
@@ -152,7 +159,6 @@ const NavBar = () => {
                 Admin
               </a>
             </li>
-            
           </ul>
         </div>
       </div>
--- project_frontend/src/pages/home/contents/Landing.js
+++ project_frontend/src/pages/home/contents/Landing.js
@@ -256,7 +256,7 @@ The Chief Minister also released the logo of the ICT Academy and Education Minis
           </div>
         </section>
 
-        <section className="pb-20 relative block bg-gray-900">
+        <section className="pb-20 relative block bg-[#151D3B]">
           <div
             className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
             style={{ height: "80px" }}
@@ -271,7 +271,7 @@ The Chief Minister also released the logo of the ICT Academy and Education Minis
               y="0"
             >
               <polygon
-                className="text-gray-900 fill-current"
+                className="text-[#151D3B] fill-current"
                 points="2560 0 2560 100 0 100"
               ></polygon>
             </svg>
@@ -336,11 +336,11 @@ The Chief Minister also released the logo of the ICT Academy and Education Minis
             </div>
           </div>
         </section>
-        <section className="relative block py-24 lg:pt-0 bg-gray-900">
+        <section className="relative block py-24 lg:pt-0 bg-[#151D3B]">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap justify-center lg:-mt-64 -mt-48">
               <div className="w-full lg:w-6/12 px-4">
-                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300">
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 mt-[4rem]">
                   <div className="flex-auto p-5 lg:p-10">
                     <h4 className="text-2xl font-semibold">
                       Want to work with us?
