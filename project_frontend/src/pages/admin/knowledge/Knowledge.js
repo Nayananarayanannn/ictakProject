bba309f830dfb006a80d2f29dea74915796363c0
Neethumar <93718958+Neethumar@users.noreply.github.com>
2022-03-11 12:14:43 +0530

    Update Knowledge.js

--- project_frontend/src/pages/admin/knowledge/Knowledge.js
+++ project_frontend/src/pages/admin/knowledge/Knowledge.js
@@ -71,9 +71,9 @@ function Knowledge() {
                             </NavLink>
                         </div>
                     </div>
-                    <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-hidden">
-                        <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                            <table className="min-w-full leading-normal overflow-hidden">
+                    <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-auto">
+                        <div className="inline-block min-w-full shadow rounded-lg overflow-auto" style={{maxHeight:"80vh"}}>
+                            <table className="min-w-full leading-normal overflow-auto">
                                 <thead>
                                     <tr>
                                         <th
@@ -153,15 +153,11 @@ function Knowledge() {
                                                                 />
                                                             </svg>
                                                         </button>
-
-
                                                     </td>
                                                 </tr>
-
                                             )
                                         })
                                     }
-
                                 </tbody>
                             </table>
                         </div>
