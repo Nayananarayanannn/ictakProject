b713eb0f181b60d2d41cbe347e9f3286458cb334
Nayana Narayanan <nayananarayanannn@gmail.com>
2022-03-12 10:59:09 +0530

    links added

--- project_frontend/src/pages/home/contents/Hero.js
+++ project_frontend/src/pages/home/contents/Hero.js
@@ -43,7 +43,7 @@ const Hero = () => {
                   className="inline-block px-7 py-3 mb-2 md:mb-0 md:mr-2 bg-[#00000A] text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
-                  href="#!"
+                  href="/courses"
                   role="button"
                 >
                   Get started
@@ -52,7 +52,7 @@ const Hero = () => {
                   className="inline-block px-7 py-3 bg-transparent text-[#036BCB] font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
-                  href="#!"
+                  href="/about-us"
                   role="button"
                 >
                   Learn more
